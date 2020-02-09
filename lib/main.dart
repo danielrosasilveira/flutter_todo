9a73375ac7c3bc94c4fcbe99a5e03c1af75c43ce
Daniel Rosa da Silveira <danielrosasilveira@gmail.com>
2020-02-08 23:10:22 -0300

    CheckboxListTile

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,9 @@ class HomePage extends StatefulWidget {
   var items = new List<Item>();
   HomePage() {
     items = [];
-    items.add(Item(title: "Item 1", done: false));
-    items.add(Item(title: "Item 2", done: true));
-    items.add(Item(title: "Item 3", done: true));
+    items.add(Item(title: "Banana", done: false));
+    items.add(Item(title: "Abacate", done: true));
+    items.add(Item(title: "Laranja", done: false));
   }
   @override
   _HomePageState createState() => _HomePageState();
@@ -38,7 +38,15 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(
         itemCount: widget.items.length,
         itemBuilder: (BuildContext ctxt, int index) {
-          return Text(widget.items[index].title);
+          final item = widget.items[index];
+          return CheckboxListTile(
+            title: Text(item.title),
+            key: Key(item.title),
+            value: item.done,
+            onChanged: (value) {
+              print(value);
+            },
+          );
         },
       ),
     );
